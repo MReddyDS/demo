e62c2bb7a9a18796c8fede38c28acf47ac167e2d
MReddyDS <muralitpcp@gmail.com>
2023-11-03 20:26:21 +0530

    updated hello.py program with square method

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ def hello():
     print("Hello world!")
 
 hello()
+
+# create a function to return square of give numeric value
+def int_square(n):
+    return n**2
+
+num = int_square(int(input("Give integer (1-9):")))
+
+print(f"Squared given integer - {num}")
+
